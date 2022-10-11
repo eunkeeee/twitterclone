f71082ec8218e83b605ae7af0ac81b837c4117e9
Eunkeee <eunkeeeee@gmail.com>
2022-10-11 23:53:19 +0900

    1.1 .env에 환경변수 넣기

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOQGdUZYSQ5Na7GBsrmQEmyPImBRzraa8",
-  authDomain: "twitterclone-49037.firebaseapp.com",
-  projectId: "twitterclone-49037",
-  storageBucket: "twitterclone-49037.appspot.com",
-  messagingSenderId: "19522966031",
-  appId: "1:19522966031:web:0a1d5e2307a017f15d497a",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
