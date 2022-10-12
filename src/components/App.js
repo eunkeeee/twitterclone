baf675e233a30e045a9fdf4b46b61e6cf9594f0e
Eunkeee <eunkeeeee@gmail.com>
2022-10-12 12:22:47 +0900

    2.0 Route에는 경로에 관련된 것만

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import AppRouter from "./Router";
+import { useState } from "react";
+import firebase from "../firebase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; Twitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
