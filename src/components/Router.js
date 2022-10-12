659be06ae78e87222e7d7a9b6f8668625d3a59bf
Eunkeee <eunkeeeee@gmail.com>
2022-10-12 14:25:07 +0900

    2.6 Navigation 만들어서 profile page도 연결
    https://stackoverflow.com/questions/71832720/link-tag-inside-browserrouter-changes-only-the-url-but-doesnt-render-the-compo/
    
    react router dom 과 react의 호환성 문제로 재설치

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Profile from "routes/Profile";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {/* && => Navigation이 존재하려면 isLoggedIn이 true여야 함. */}
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </>
         ) : (
