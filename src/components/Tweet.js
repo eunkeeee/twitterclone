1dd392c20f63506f5b2829ab6228e2831155cc86
Eunkeee <eunkeeeee@gmail.com>
2022-10-13 14:00:02 +0900

    3.5 Delete Tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,19 @@
+import { dbService } from "fbase";
 import React from "react";
 
 const Tweet = ({ key, tweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure, you want to delete this Tweet?");
+    if (ok) {
+      await dbService.doc(`tweets/${tweetObj.id}`).delete();
+    }
+  };
   return (
     <div>
       <h4>{tweetObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete Tweet</button>
+          <button onClick={onDeleteClick}>Delete Tweet</button>
           <button>Edit Tweet</button>
         </>
       )}
