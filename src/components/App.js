cdf2f0866d1d5739ace2834c235ac23d136c1e67
Eunkeee <eunkeeeee@gmail.com>
2022-10-14 16:23:50 +0900

    5.2 해결책1: 몇 가지만 특정해서 수정한다.

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,23 @@ function App() {
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       } else {
       }
       setInit(true);
     });
   }, []);
   const refreshUser = () => {
-    // 우리가 늘 사용한 userobj => 통일성을 위한 state지만,
-    // firebase에 user를 수정했다고 해도 React와 연결되어 있지 않음...
-    // firebase에 정보를 가지고 react JS를 업데이트 해줘야 함!!!
-    // 이건 그 업데이트를 해주기 위한 function으로 App(상류)부터 필요한 모든 곳에 들어감.
-    setUserObj(authService.currentUser);
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
   };
   return (
     <>
