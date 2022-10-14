6785f3836306acc295614d4585c9e089de5a7c70
Eunkeee <eunkeeeee@gmail.com>
2022-10-14 16:08:42 +0900

    5.1 .updateProfile로 displayName을 바꿀 수 있음 but 바로 업데이트가 되지 않는 문제...!

--- src/components/Router.js
+++ src/components/Router.js
@@ -14,7 +14,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
       {/* && => Navigation이 존재하려면 isLoggedIn이 true여야 함. */}
-      {isLoggedIn && <Navigation />}
+      {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
           <>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
 
 const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
@@ -12,15 +13,35 @@ const Profile = ({ userObj }) => {
     const tweets = await dbService
       .collection("tweets")
       .where("creatorId", "==", userObj.uid)
-      .orderBy("createdAt")
       .get();
     console.log(tweets.docs.map((doc) => doc.data));
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({ displayName: newDisplayName });
+    }
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
   };
   useEffect(() => {
     getMyTweets();
   }, []);
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+          onChange={onChange}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
