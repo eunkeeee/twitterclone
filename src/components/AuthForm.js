ad9e10a95ea30f82ff2dc202ebecfb670d67241b
Eunkeee <eunkeeeee@gmail.com>
2022-10-15 10:47:12 +0900

    6.0 코드정리

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,12 +1,12 @@
-import AuthForm from "components/AuthForm";
+import { authService } from "fbase";
 import React, { useState } from "react";
-import { firebaseInstance, authService } from "../fbase";
 
-const Auth = () => {
+const AuthForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
   const [error, setError] = useState("");
+
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -39,32 +39,36 @@ const Auth = () => {
   const toggleAccout = () => {
     setNewAccount((prev) => !prev);
   };
-  const onSocialClick = async (event) => {
-    const {
-      target: { name },
-    } = event;
-    let provider;
-    if (name === "google") {
-      provider = new firebaseInstance.auth.GoogleAuthProvider();
-    } else if (name === "github") {
-      provider = new firebaseInstance.auth.GithubAuthProvider();
-    }
-    const data = await authService.signInWithPopup(provider);
-    console.log(data);
-  };
   return (
-    <div>
-      <AuthForm />
-      <div>
-        <button name="google" onClick={onSocialClick}>
-          Continue with Google
-        </button>
-        <button name="github" onClick={onSocialClick}>
-          Continue with GitHub
-        </button>
-      </div>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create Accout" : "Log In"}
+          required
+        />
+        {error}
+      </form>
+      <span onClick={toggleAccout}>
+        {!newAccount ? "Doens't have an account?" : "Already have an account?"}
+      </span>
+    </>
   );
 };
-
-export default Auth;
+export default AuthForm;
